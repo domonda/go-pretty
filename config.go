d049458bd013763b86af4c583f1e0920aa44c36d
Erik Unger <erik@erikunger.com>
2026-01-11 16:34:15 +0100

    Update Go version to 1.22 and refactor config.go for improved readability and organization of constants and variables.

--- config.go
+++ config.go
@@ -5,20 +5,22 @@ import (
 	"time"
 )
 
-// DefaultPrinter is used by the package level print functions
-var DefaultPrinter = Printer{
-	MaxStringLength: 200,
-	MaxErrorLength:  2000,
-	MaxSliceLength:  20,
-}
+var (
+	// DefaultPrinter is used by the package level print functions
+	DefaultPrinter = Printer{
+		MaxStringLength: 200,
+		MaxErrorLength:  2000,
+		MaxSliceLength:  20,
+	}
 
-// CircularRef is a replacement token CIRCULAR_REF
-// that will be printed instad of a circular data reference.
-const CircularRef = "CIRCULAR_REF"
+	// CircularRef is a replacement token (default "CIRCULAR_REF")
+	// that will be printed instad of a circular data reference.
+	CircularRef = "CIRCULAR_REF"
+)
 
 var (
-	typeOfByte     = reflect.TypeOf(byte(0))
-	typeOfRune     = reflect.TypeOf(rune(0))
-	typeOfTime     = reflect.TypeOf(time.Time{})
-	typeOfDuration = reflect.TypeOf(time.Duration(0))
+	typeOfByte     = reflect.TypeFor[byte]()
+	typeOfRune     = reflect.TypeFor[rune]()
+	typeOfTime     = reflect.TypeFor[time.Time]()
+	typeOfDuration = reflect.TypeFor[time.Duration]()
 )
